ef06f494a83449cf239ef9a96ab37c9a7f5725c5
shagmbg <michael.b.gottfried@gmail.com>
2013-10-30 13:00:55 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Cookbook::Application.config.secret_key_base = '617ccb0a0c1970be9881a7a053b2f6b9a2d395037b3949e0b05155904c32f488c50202bbed52ab4425f925f1af24aa4525372a4a76bf4166cecb92f1fd32e6fb'
