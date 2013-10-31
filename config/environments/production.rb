43447cb3c0581950c5c7b74d0e501013cdb4fffd
shagmbg <michael.b.gottfried@gmail.com>
2013-10-31 10:07:14 -0400

    Added Devise and updated home and header routes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,7 @@ Cookbook::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  #Set to actual production environment
+  config.action_mailer.default_url_options = { :host => 'THE URL' }
 end
