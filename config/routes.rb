1f5f7b0eb7561c0c20a9d4e147140b370419f65a
shagmbg <michael.b.gottfried@gmail.com>
2013-11-01 11:41:32 -0400

    Updated show view, added glyphicons, changed home root path to include recipes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Cookbook::Application.routes.draw do
   resources :recipes
 
   devise_for :users
-  root "pages#home"
+  root "recipes#index"
   get "about" => "pages#about"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
