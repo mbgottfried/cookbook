c6655bbc80388e801d4ec5a9a358ddbde78c2c6b
shagmbg <michael.b.gottfried@gmail.com>
2013-11-01 12:49:31 -0400

    pushing before attempting to get home page outside of container

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131101141853) do
+ActiveRecord::Schema.define(version: 20131101160302) do
 
   create_table "recipes", force: true do |t|
     t.string   "title"
@@ -40,6 +40,7 @@ ActiveRecord::Schema.define(version: 20131101141853) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "username"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
