6991da6a5ce652491c4fe5f4b1af5d4c4c08a5ce
shagmbg <michael.b.gottfried@gmail.com>
2013-11-01 10:38:58 -0400

    added Paperclip and images to recipes

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131031195703) do
+ActiveRecord::Schema.define(version: 20131101141853) do
 
   create_table "recipes", force: true do |t|
     t.string   "title"
@@ -19,6 +19,10 @@ ActiveRecord::Schema.define(version: 20131031195703) do
     t.datetime "updated_at"
     t.integer  "user_id"
     t.string   "ingredients"
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
   end
 
   add_index "recipes", ["user_id"], name: "index_recipes_on_user_id"
